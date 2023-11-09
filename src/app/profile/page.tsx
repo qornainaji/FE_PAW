72b5de77d3a7b33acf988d6c809cbe4aef80c007
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-11-10 00:25:31 +0700

    feat: added signout and locked home page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,12 +2,14 @@ import { options } from '../api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import React from 'react';
+import Link from 'next/link';
 
 export default async function ServerPage() {
     const session = await getServerSession(options);
 
     if (!session) {
         redirect('/api/auth/signin?callbackUrl=http://localhost:3000/profile')
+        return null;
     }
 
     return (
@@ -16,6 +18,11 @@ export default async function ServerPage() {
             <p className='mt-5'>Welcome {session.user?.name}</p>
             <p className='mt-5'>Email: {session.user?.email}</p>
             <p className='mt-5'>Image: {session.user?.image}</p>
+            
+            <Link legacyBehavior href='/api/auth/signout'>
+                <a className='mt-5 text-blue-500'>Sign Out</a>
+            </Link>
+            
         </section>
     )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,4 +4,4 @@
 export { default } from 'next-auth/middleware'
 
 // Applies next-auth only to matching routes
-export const config = { matcher: ["/api/auth", "/dashboard"] }
+export const config = { matcher: ["/api/auth", "/dashboard", "/"] }
