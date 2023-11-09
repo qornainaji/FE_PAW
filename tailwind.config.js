1d1223198ba45ccdb0c90c81c78530823a9bfd82
qornainaji <qornian.aji@gmail.com>
2023-11-09 16:39:42 +0700

    ADD: menambahkan quicksand dan color pada tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-quicksand)'],
+      },
+      colors: {
+        'neutral': {
+          DEFAULT: '#FFFFFF',
+          50: '#F6F6F6',
+          100: '#EEEEEE',
+          300: '#BBBFC8',
+          500: '#7F838E',
+          700: '#4F525B',
+          900: '#242730',
+          1000: '#0E111A'
+        },
+        "extra-neutral": '#181C25',
+        "blue-x": '#DAE6FD',
+        "blue-y": '#5885F0',
+        "blue-z": '#163BB8',
+        "error": '#FF4D4F',
+        "credit": '#t'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
