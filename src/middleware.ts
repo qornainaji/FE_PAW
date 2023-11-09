518f8aeb70a4dd9c32b39e86069e14694bda8e2e
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-11-09 22:32:36 +0700

    feat: added GIthub Oauth login to all of the pages
    Disable by commenting out the line in src/middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,6 @@
+// Without a defined matcher, this one line applies next-auth to all routes in the app.
+//
+
+// Comment out the following line to disable login for all routes
+
+export { default } from 'next-auth/middleware'
