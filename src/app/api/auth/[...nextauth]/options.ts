1e7da42ad11af2932d3ef2f4134f56b7c0cf14cc
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-11-10 07:12:39 +0700

    feat: credentials login working + logging
    TODO: Comment loggings

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -21,29 +21,46 @@ export const options: NextAuthOptions = {
                 password: { label: 'Password', type: 'password', placeholder: 'Hush, it\'s a secret!' },
             },
             async authorize(credentials) {
-                const uri = process.env.MONGODB_URI;
-                const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true } as any);
+                let client: MongoClient | null = null;
+                console.log('credentials', credentials);
 
                 try {
-                    console.log('Connecting to MongoDB...');
+                    console.log('Connecting to MongoDB');
+                    // MONGO_URI in .env.local
+                    const uri = process.env.MONGO_URI as string;
+                    console.log('uri', uri);
+                    const client = new MongoClient(uri, {
+                        useNewUrlParser: true,
+                        useUnifiedTopology: true,
+                    } as any);
+                    console.log('client', client);
+
                     await client.connect();
-                    console.log('Connected to MongoDB!');
+                    console.log('Connected to MongoDB');
+                    console.log('credentials', credentials);
 
-                    // Use the appropriate collection and find query based on your MongoDB setup
-                    const user = await client.db('PAW').collection('users').findOne({
-                        username: credentials?.username,
-                        password: credentials?.password,
-                    });
-
+                    const user = await client
+                        .db('PAW')
+                        .collection('users')
+                        .findOne({
+                            user_name: credentials.username,
+                            user_password: credentials.password,
+                        });
+            
                     if (user) {
                         return Promise.resolve(user);
                     } else {
+                        console.log('User not found');
                         return Promise.resolve(null);
                     }
-                } finally {
-                    await client.close();
+                }   catch (error) {
+                    return Promise.resolve(null);
+                }   finally {
+                    if (client) {
+                        await client.close();
+                    }
                 }
-            },
+              },
         }),
     ],
 }
