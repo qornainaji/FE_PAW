618a652d2f305f0a13aadb9028979c7e7d6aea5e
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-11-10 09:35:28 +0700

    feat: backend api added in env and added login & register

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,21 +1,45 @@
+"use client";
+import axios from 'axios';
+import React from 'react';
+import {useState} from 'react';
+
 export default function LoginPage() {
+    const [nama, setNama] = useState("");
+    const [password, setPassword] = useState("");
     return(
-        <main className="min-h-screen flex justify-center items-center bg-[#F5E1D3] text-black font-sans">
+        <main className="min-h-screen flex justify-center items-center bg-orange-100 text-black font-sans">
             <div className="bg-white p-10 rounded-[20px] flex flex-col gap-5">
                 <div>
                     <h1 className="text-[30px] text-center">Login</h1>
-                    <p>Short Description</p>
+                    <p>Ready to learn?</p>
                 </div>
-                <form className="flex flex-col gap-4">
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    console.log(
+                        {
+                            user_name: nama,
+                            user_password: password
+                        }
+                    )
+                    axios.post("http://localhost:4000/" + "users/login", {
+                        user_name: nama,
+                        user_password: password
+                    }).then(() => {
+                        alert(`success, welcome ${nama}`)
+                    }).catch((err) => {
+                        console.log(err.message)
+                        alert("gagal")
+                    })
+                }} className="flex flex-col gap-4">
                     <label className="flex flex-col gap-2">
                         Nama/Email
-                        <input type="text" className="outline" />
+                        <input type="text" className="outline" value={nama} onChange={(e) => {setNama(e.target.value)}} />
                     </label>
                     <label className="flex flex-col gap-2">
                         Password
-                        <input type="password" className="outline" />
+                        <input type="password" className="outline" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </label>
-                    <button className="mt-2 block bg-green-2-500 text-white py-2 rounded-full">Masuk</button>
+                    <button type="submit" className="mt-2 block bg-green-2-500 text-white py-2 rounded-full">Masuk</button>
                 </form>
             </div>
         </main>
--- src/app/auth/new-user/page.tsx
+++ src/app/auth/new-user/page.tsx
@@ -1,28 +1,50 @@
 // pages/new-user.tsx
+"use client";
 import React from 'react';
+import axios from 'axios';
+import {useState} from 'react';
 
 const NewUserPage: React.FC = () => {
-  return (
-    <main className="min-h-screen flex justify-center items-center bg-[#F5E1D3] text-black font-sans">
-        <div className="bg-white p-10 rounded-[20px] flex flex-col gap-5">
-            <div>
-                <h1 className="text-[30px] text-center">Login</h1>
-                <p>Short Description</p>
+    const [nama, setNama] = useState("");
+    const [password, setPassword] = useState("");
+    return(
+        <main className="min-h-screen flex justify-center items-center bg-orange-100 text-black font-sans">
+            <div className="bg-white p-10 rounded-[20px] flex flex-col gap-5">
+                <div>
+                    <h1 className="text-[30px] text-center">Sign Up</h1>
+                    <p>Register and Learn!</p>
+                </div>
+                <form onSubmit={(e) => {
+                    e.preventDefault();
+                    console.log(
+                        {
+                            user_name: nama,
+                            user_password: password
+                        }
+                    )
+                    axios.post("http://localhost:4000/" + "users/login", {
+                        user_name: nama,
+                        user_password: password
+                    }).then(() => {
+                        alert(`success, welcome ${nama}`)
+                    }).catch((err) => {
+                        console.log(err.message)
+                        alert("gagal")
+                    })
+                }} className="flex flex-col gap-4">
+                    <label className="flex flex-col gap-2">
+                        Nama/Email
+                        <input type="text" className="outline" value={nama} onChange={(e) => {setNama(e.target.value)}} />
+                    </label>
+                    <label className="flex flex-col gap-2">
+                        Password
+                        <input type="password" className="outline" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                    <button type="submit" className="mt-2 block bg-green-2-500 text-white py-2 rounded-full">Masuk</button>
+                </form>
             </div>
-            <form className="flex flex-col gap-4">
-                <label className="flex flex-col gap-2">
-                    Nama/Email
-                    <input type="text" className="outline" />
-                </label>
-                <label className="flex flex-col gap-2">
-                    Password
-                    <input type="password" className="outline" />
-                </label>
-                <button className="mt-2 block bg-green-2-500 text-white py-2 rounded-full">Masuk</button>
-            </form>
-        </div>
-    </main>
-  );
+        </main>
+    )
 };
 
 export default NewUserPage;
