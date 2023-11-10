f9ff810429870b09fc4cbd01ac18173900a6a8f4
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-11-10 07:17:44 +0700

    feat: added signout button

--- src/app/page.js
+++ src/app/page.js
@@ -109,6 +109,24 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
+        {/* Sign Out Button */}
+        <a
+          href="/api/auth/signout"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Sign Out{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Sign out of the app.
+          </p>
+        </a>
       </div>
     </main>
   )
