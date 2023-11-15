c298442291e66cf660941a6033c65a06b6bfc5a6
qornainaji <qornian.aji@gmail.com>
2023-11-15 13:25:07 +0700

    EDIT: Search bar shadow

--- src/app/components/searchbar/searchbar.js
+++ src/app/components/searchbar/searchbar.js
@@ -6,11 +6,11 @@ import { IoFilter } from "react-icons/io5";
 
 export default function SearchBar(){
     return(
-        <div className="w-full flex justify-center h-full font-sans">
+        <div className="w-full flex justify-center h-full font-sans drop-shadow-[0_12px_20px_rgba(220,155,107,0.24)]">
             <form className="flex w-[90%] max-w-[794px] py-[18px] bg-neutral pl-[20px] rounded-full pr-[8px] relative items-center">
                 <input
                 type="text" 
-                className="w-full font-medium text-[16px] text-neutral-500 placeholder:text-neutral-500"
+                className="w-full font-medium text-[16px] text-neutral-900 placeholder:text-neutral-500 focus:outline-none"
                 placeholder="Cari dokumen kamu disini..."
                 />
                 <Button
