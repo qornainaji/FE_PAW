2b2bc4ae716079ad679ddffacb0f143054d45d9d
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2023-11-17 07:39:41 +0700

    feat: added JWT_SECRET

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -37,31 +37,36 @@ import Image from 'next/image';
 import styles from '../../globals.css';
 
 export default function Navbar({ isAdmin }) {
+    // Decode the JWT to get the payload
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    // The user ID should be available in the decoded token
+    const userId = decodedToken._id;
       
-  return (
-    <div className='sticky-navbar w-full'>
-        <nav className='flex font-sans text-xl text-neutral-500 justify-between items-center px-[120px] py-[24px] bg-orange-100'>
-            {/* <h1 className='font-bold text-green-1-900'>ACADEMIA DTETI</h1> */}
-            <Image src='/images/AcademiaDTETI.png' alt='Logo' width={195.2} height={32} className=''/>
-            {isAdmin ? (
-                <ul className='flex font-semibold text-[16px] space-x-[72px]'>
-                <li className='hover:text-green-2-600 transition-colors'>
-                    <Link href='/'>Beranda</Link>
-                </li>
-                <li className='hover:text-green-2-600 transition-colors'>
-                    <Link href='/dashboard'>Dashboard</Link>
-                </li>
-                <li className='hover:text-green-2-600 transition-colors'>
-                    <Link href='/verifikasi'>Verifikasi</Link>
-                </li>
-                </ul>
-            ) : null}
-            <div className='flex space-x-[8px]'>
-                <p className='text-[16px] font-semibold text-green-1-900'>Nama Pengguna</p>
-                <p className='w-[32px] h-[32px] rounded-full bg-green-600'></p>
-            </div>
-        </nav>
-    </div>
+    return (
+        <div className='sticky-navbar w-full'>
+            <nav className='flex font-sans text-xl text-neutral-500 justify-between items-center px-[120px] py-[24px] bg-orange-100'>
+                {/* <h1 className='font-bold text-green-1-900'>ACADEMIA DTETI</h1> */}
+                <Image src='/images/AcademiaDTETI.png' alt='Logo' width={195.2} height={32} className=''/>
+                {isAdmin ? (
+                    <ul className='flex font-semibold text-[16px] space-x-[72px]'>
+                    <li className='hover:text-green-2-600 transition-colors'>
+                        <Link href='/'>Beranda</Link>
+                    </li>
+                    <li className='hover:text-green-2-600 transition-colors'>
+                        <Link href='/dashboard'>Dashboard</Link>
+                    </li>
+                    <li className='hover:text-green-2-600 transition-colors'>
+                        <Link href='/verifikasi'>Verifikasi</Link>
+                    </li>
+                    </ul>
+                ) : null}
+                <div className='flex space-x-[8px]'>
+                    <p className='text-[16px] font-semibold text-green-1-900'>Nama Pengguna</p>
+                    <p className='w-[32px] h-[32px] rounded-full bg-green-600'></p>
+                </div>
+            </nav>
+        </div>
   );
 //  return (
 //     <nav className={`${styles.navbar} ${isSticky ? styles.sticky : ''}`}>
