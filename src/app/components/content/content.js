cde4fb4b5c155fff1ec7d900602db4d68aa2dcf6
qornainaji <qornian.aji@gmail.com>
2023-11-21 10:15:23 +0700

    EDIT: Content filter belum condition and onchange handling

--- src/app/components/content/content.js
+++ src/app/components/content/content.js
@@ -1,5 +1,6 @@
 import Button from "../button/button";
 import SearchBar from "../searchbar/searchbar";
+import Filter from "../filter/filter";
 import { IoFilter } from "react-icons/io5";
 
 export default function Content(){
@@ -7,9 +8,9 @@ export default function Content(){
         <div className="flex flex-col items-center justify-between bg-orange-100 py-[60px] px-[120px] font-sans text-neutral-1000 ">
             <h1 className="font-bold text-[38px] mb-[24px] ">Cari dokumen kamu di bawah!</h1>
             <SearchBar/>
-            <div className="flex flex-row items-center justify-between w-full mt-[40px]">
+            <div className="flex flex-row items-center justify-between w-full mt-[40px] mb-[24px]">
                 <div className="flex space-x-[119px]">
-                    <p className="font-bold text-[16px] text-neutral-1000 py-[8px] px-[10px] ">Populer:</p>
+                    <p className="font-bold text-[16px] text-neutral-1000 py-[8px] pr-[20px] ">Populer:</p>
                         <div>
                             <button 
                             className="px-[24px] py-[10px] font-semibold text-[14px] truncate text-neutral-500 hover:text-green-2-500 max-w-[200px] hover:bg-neutral transition-colors rounded-[12px] hover:drop-shadow-[0_12px_20px_rgba(220,155,107,0.12)]">
@@ -25,6 +26,7 @@ export default function Content(){
                     picture={<IoFilter className="text-green-2-500 group-hover:text-green-1-500"/>}
                 />
             </div>
+            <Filter/>
         </div>
     )
 }
---
+++ src/app/components/filter/filter.js
@@ -0,0 +1,51 @@
+'use client';
+import { ConfigProvider, Select, Space } from "antd";
+
+
+export default function Filter(){
+    return(
+        <ConfigProvider 
+        theme={{
+            token: {
+              // Seed Token
+              colorPrimary: '#48A516',
+            },
+          }}
+        >
+            <div className="flex flex-row pb-[24px] w-full space-x-[20px] text-neutral-900 ">
+                <div className="flex flex-row space-x-[8px] ">
+                    <p className="font-medium">Program Studi :</p>
+                    <Space wrap>
+                        <Select
+                            placeholder="e.g. Teknologi Informasi"
+                            style={{ width: 296, borderColor: '#48A516' }}
+                            
+                            // onChange={{/*onChange*/}}
+                            options={[
+                                {value: 'tif', label: 'Teknologi Informasi'},
+                                {value: 'te', label: 'Teknik Elektro'},
+                                {value: 'tb', label: 'Teknik Biomedis'},
+                            ]}
+                        />
+                    </Space>
+                </div>
+                <div className="flex flex-row space-x-[8px] ">
+                    <p className="font-medium">Program Studi :</p>
+                    <Space wrap>
+                        <Select
+                            placeholder="2023"
+                            style={{ width: 296, borderColor: '#48A516' }}
+                            
+                            // onChange={{/*onChange*/}}
+                            options={[
+                                {value: 2023, label: '2023'},
+                                {value: 2022, label: '2022'},
+                                {value: 2021, label: '2021'},
+                            ]}
+                        />
+                    </Space>
+                </div>
+            </div>
+        </ConfigProvider>
+    )
+}
