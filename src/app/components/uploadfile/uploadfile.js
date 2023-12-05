a0e3d67cdc167407b75452b21df35575db4e1d95
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-12-05 13:35:06 +0700

    feat: disabled body scroll when modal is opened
    also adjusted modal height

--- src/app/components/uploadfile/uploadfile.js
+++ src/app/components/uploadfile/uploadfile.js
@@ -6,7 +6,6 @@ import { message, Upload } from 'antd';
 const { Dragger } = Upload;
 import Button from '../button/button';
 import axios from 'axios';
-import { on } from 'events';
 import { use, useState, useEffect, useRef } from 'react';
 
 
@@ -15,21 +14,57 @@ export default function UploadFile({ onClose }) {
     const [pdfPreview, setPdfPreview] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    // Ref for modal container
     const modalContainerRef = useRef(null);
 
+    // Close modal when clicked outside of modal container
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (modalContainerRef.current && !modalContainerRef.current.contains(event.target)) {
                 onClose();
+                enableBodyScroll();
             }
         }
         
         document.addEventListener("mousedown", handleClickOutside);
+        disableBodyScroll();
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
+            enableBodyScroll();
         }
+    }, [modalContainerRef, onClose]);
+
+    // Disable body scroll when modal is open
+    const disableBodyScroll = () => {
+        document.body.style.overflow = 'hidden';
+    }
+
+    // Enable body scroll when modal is closed
+    const enableBodyScroll = () => {
+        document.body.style.overflow = 'unset';
+    }
+
+    // Change cursor to pointer when mouse is outside of modal
+    useEffect(() => {
+        const handleCursorChange = (event) => {
+            const isMouseOutsideModal = modalContainerRef.current && !modalContainerRef.current.contains(event.target);
+            const isModalOpen = document.body.style.overflow == 'hidden';
+
+            if (isMouseOutsideModal && isModalOpen) {
+                document.body.style.cursor = 'pointer';
+            } else {
+                document.body.style.cursor = ''; // Reset to default cursor when inside the modal or when it's closed
+            }
+        };
+
+        document.addEventListener("mousemove", handleCursorChange);
+
+        return () => {
+            
+        };
     }, [modalContainerRef]);
 
+    // Handle file preview
     const onFileChange = (e) => {
         const file = e.target.files[0];
         const reader = new FileReader();
@@ -135,7 +170,7 @@ export default function UploadFile({ onClose }) {
 
     return (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50" >
-            <div ref={modalContainerRef} className="bg-white p-10 mt-10 rounded-lg flex flex-row-reverse h-3/4 overflow-auto">
+            <div ref={modalContainerRef} className="bg-white p-10 mt-16 rounded-lg flex flex-row-reverse h-5/6 overflow-auto">
                 <div>
                     <div>
                         <h1 className="text-[30px] font-bold text-neutral-900 pb-[8px] ">Unggah Dokumen Anda!</h1>
