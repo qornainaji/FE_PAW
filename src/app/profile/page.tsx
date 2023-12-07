66f327e8ae303528612d5ad7186836a660bdca1a
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-12-08 01:39:54 +0700

    feat: change password is live

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,14 +5,16 @@ import Navbar from '../components/navbar/navbar';
 import React, {useState, useEffect} from 'react';
 import cookieCutter from 'cookie-cutter';
 import jwt from 'jsonwebtoken';
-import axios from 'axios';
-import { Form, Input, Button, Typography, ConfigProvider, Image } from 'antd';
+import axios, { all } from 'axios';
+import { Form, Input, Button, Typography, ConfigProvider, Image, message } from 'antd';
 import FadeIn from '../animations/FadeIn';
 import Head from 'next/head';
 import { checkAuthentication } from '../auth/checkAuthentication';
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import LoadingScreen from '../components/loadingScreen/loadingScreen';
+
+const bcrypt = require('bcryptjs');
 
 const { Title } = Typography;
 
@@ -23,7 +25,7 @@ const Profile = () => {
     const [id, setId] = useState(null);
     const [username, setUsername] = useState(null);
     const [name, setName] = useState(null);
-    const [password, setPassword] = useState(null);
+    // const [password, setPassword] = useState(null);
     const [email, setEmail] = useState(null);
     const [NIM, setNIM] = useState(null);
     const [github, setGithub] = useState(null);
@@ -31,6 +33,9 @@ const Profile = () => {
     const [location, setLocation] = useState(null);
     const [isVerified, setIsVerified] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    const [changePasswordState, setChangePasswordState] = useState(false);
+    const [newPassword, setNewPassword] = useState(null);
 
     useEffect(() => {
         checkAuthentication( router );
@@ -69,13 +74,6 @@ const Profile = () => {
     }
     }, [token])
 
-    // function to show success toast
-    const successToast = (message) => {
-        toast.success(message, {
-            position: toast.POSITION.TOP_CENTER
-        });
-    };
-
 
     const validateUgmEmail = (rule, value) => {
         if (value && (!value.includes("@mail.ugm.ac.id") || value.includes("@ugm.ac.id") || value.includes("@365.ugm.ac.id"))) {
@@ -106,7 +104,7 @@ const Profile = () => {
             });
             
             // Alert the whole data
-            // alert(JSON.stringify(response.data));
+            alert(JSON.stringify(response.data));
 
             alert("Profile updated successfully!")
 
@@ -121,6 +119,54 @@ const Profile = () => {
                 console.error('An error occurred during registration:', error);
                 alert('An unexpected error occurred. Please try again later.');
             }
+        };
+    };
+
+    const handlePasswordChanged = async ( newPassword ) => {
+        message.loading({ content: 'Updating Password...', key: 'passwordChange' });
+        const salt = await bcrypt.genSalt(10);
+        var hashedPassword = await bcrypt.hash(newPassword, salt);
+        const data = {
+            _id: id,
+            user_password: hashedPassword,
+        };
+
+        try {
+            // Update the data
+            const response = await axios.patch(process.env.NEXT_PUBLIC_API_URL + 'users/' + id, data, {
+                headers: { 'Authorization': `Bearer ${token}` }
+            });
+
+            if (response.data.user_password == hashedPassword) {
+                message.destroy('passwordChange');
+                message.success({ content: 'Password updated successfully!', key: 'passwordChange', duration: 2 });
+                alert("Password updated successfully!")
+                // alert("Password updated successfully!\n" + "Sent password: " + hashedPassword + "\nCurr password: " + response.data.user_password)
+                
+                // use toast
+                toast.success("Password updated successfully!", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+                setChangePasswordState(false);
+            } else {
+                message.destroy('passwordChange');
+                message.error({ content: 'Password update failed!', key: 'passwordChange', duration: 2 });
+                alert("Password update failed!")
+                // alert("Password update failed!\n" + "Sent password: " + hashedPassword + "\nCurr password: " + response.data.user_password)
+            }
+
+            // alert(JSON.stringify(response.data));
+
+        } catch(error) {
+            if (error.response && error.response.status === 400) {
+                // successToast(error.response.data.message);
+                alert(error.response.data.error);
+
+            } else {
+                console.error('An error occurred during password change', error);
+                alert('An unexpected error occurred. Please try again later.');
+            }
+            message.error({ content: 'Password update failed!', key: 'passwordChange', duration: 2 });
         };
     };
 
@@ -225,10 +271,68 @@ const Profile = () => {
                                         name="password"
                                         initialValue=""
                                     >
-                                        <Input.Password disabled={true} placeholder='See "Change Password"'/>
-                                        <Link href="/profile/change-password">
+                                        <Input.Password disabled={true} placeholder="It's a Secret!"/>
+                                        {/* Change password button */}
+                                        <Button className='bg-green-2-500 rounded-full mb-5 font-bold mt-5 text-neutral-100 hover:!text-neutral-100 hover:bg-red-500 hover:border-red-600' onClick={() => setChangePasswordState(!changePasswordState)}>
                                             Change Password
-                                        </Link>
+                                        </Button>
+
+                                        {/* New password form */}
+                                        {changePasswordState ? (
+                                            <div className='bg-neutral-100 p-10 rounded-lg'>
+                                                <Form.Item
+                                                    label="New Password"
+                                                    name="newPassword"
+                                                    initialValue=""
+                                                    labelCol={{ span: 24 }}
+                                                    wrapperCol={{ span: 24 }}
+                                                    rules={[
+                                                        { required: true, message: 'Please input your new password!' },
+                                                        { min: 8, message: 'Password must be at least 8 characters!' },
+                                                        { max: 20, message: 'Password must be less than 20 characters!' },
+                                                    ]}
+                                                >
+                                                    <Input.Password disabled={false} onChange={(e) => setNewPassword(e.target.value)}/>
+                                                </Form.Item>
+                                                <Form.Item
+                                                    label="Confirm New Password"
+                                                    name="confirmNewPassword"
+                                                    initialValue=""
+                                                    labelCol={{ span: 24 }}
+                                                    wrapperCol={{ span: 24 }}
+                                                    rules={[
+                                                        { required: true, message: 'Please input your new password!' },
+                                                        { min: 8, message: 'Password must be at least 8 characters!' },
+                                                        { max: 20, message: 'Password must be less than 20 characters!' },
+                                                        ({ getFieldValue }) => ({
+                                                            validator(_, value) {
+                                                                if (!value || getFieldValue('newPassword') === value) {
+                                                                    return Promise.resolve();
+                                                                }
+                                                                return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                                                            },
+                                                        }),
+                                                    ]}
+                                                >
+                                                    <Input.Password disabled={false}/>
+                                                </Form.Item>
+                                                {/* Change password button */}
+                                                <Button className='bg-red-600 
+                                                                    rounded-full mb-5 
+                                                                    font-bold mt-1 
+                                                                    text-neutral-100 
+                                                                    hover:!text-neutral-100 
+                                                                    hover:bg-red-500 
+                                                                    hover:border-red-600' 
+                                                                    onClick={() => {
+                                                                        handlePasswordChanged(newPassword);
+                                                                    }}>
+                                                    Confirm Change Password
+                                                </Button>
+                                            </div>
+                                        ) : (
+                                            <></>
+                                        )}
                                     </Form.Item>
 
                                     <Form.Item
