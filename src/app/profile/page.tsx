a9681550d8b7e2ac2e3e06954a3b8f651b3b1374
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2023-12-07 19:03:48 +0700

    feat: added loading screen to profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,13 +7,12 @@ import cookieCutter from 'cookie-cutter';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
 import { Form, Input, Button, Typography, ConfigProvider, Image } from 'antd';
-import CustomAlert from '../components/CustomAlert/CustomAlert';
-import { GithubOutlined } from '@ant-design/icons';
 import FadeIn from '../animations/FadeIn';
 import Head from 'next/head';
 import { checkAuthentication } from '../auth/checkAuthentication';
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import LoadingScreen from '../components/loadingScreen/loadingScreen';
 
 const { Title } = Typography;
 
@@ -49,7 +48,6 @@ const Profile = () => {
             try {
                 axios.get(process.env.NEXT_PUBLIC_API_URL + 'users/' + decodedToken._id, { headers: { 'Authorization': `Bearer ${token}` } })
                 .then((response) => {
-                    // console.log(response.data.user_name);
                     if (response.data.user_avatarURL != null)
                         setAvatar(response.data.user_avatarURL);
                     setUsername(response.data.user_username);
@@ -127,169 +125,175 @@ const Profile = () => {
     };
 
     return (
-        <div className='flex flex-col font-sans bg-orange-100 text-neutral-1000 w-auto h-fill min-h-screen'>
-            <div className='flex flex-col h-fit bg-orange-100 text-neutral-1000'>
-                <Head>
-                    <title>Profile</title>
-                    <meta name="description" content="Profile" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                <Navbar isAdmin={true}/>
-            </div>
-
-            <div className='flex justify-center align-middle items-center w-fill'>
-                {/* Forms */}
-                <div className="flex flex-col items-center justify-center w-5/6 h-fit min-h-full mb-10 py-7 bg-white rounded-2xl">
-                    <h2 className="text-4xl font-bold mb-5">Profile</h2>
-                    {loading ? (
-                        <div className='text-2xl mt-5'>Loading . . . </div>
-                    ) : (
-                        <Form
-                            name="basic"
-                            labelCol={{ span: 24 }}
-                            wrapperCol={{ span: 24 }}
-                            initialValues={{ remember: true }}
-                            onValuesChange={(changedValues, allValues) => {
-                                console.log(allValues);
-                                setEmail(allValues.email);
-                                setNIM(allValues.NIM);
-                                setUsername(allValues.username);
-                                setName(allValues.name);
-                                setBio(allValues.bio);
-                                setLocation(allValues.location);
-                                setGithub(allValues.githubURL);
-                            }}
-                        >
-                            <ConfigProvider
-                                theme={{
-                                    token: {
-                                        colorPrimary: '#48A516',
-                                    },
-                                }}
-                            >
-                            {/* Avatar */}
-                            <Form.Item
-                                name="avatar"
-                                initialValue={avatar}
-                                className='flex justify-center'
-                            >
-                                {/* Rounded Image */}
-                                <Image
-                                    src={avatar}
-                                    alt="Avatar"
-                                    width={200}
-                                    height={200}
-                                    className="rounded-full justify-center align-middle text-center flex items-center"
-                                />
-                                <style jsx global>{`
-                                    .ant-image-mask {
-                                        border-radius: 50%;
-
-                                    }
-                                `}</style>
-                            </Form.Item>
-                
-                            
-                            {/* Create forms pre-filled with the data from the user */}
-                            <Form.Item
-                                label="ID"
-                                name="id"
-                                initialValue={id}
-                            >
-                                <Input disabled={true}/> 
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Username"
-                                name="username"
-                                initialValue={username}
-                            >
-                                <Input disabled={false}/>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Name"
-                                name="name"
-                                initialValue={name}
-                            >
-                                <Input disabled={false}/>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Password"
-                                name="password"
-                                initialValue=""
-                            >
-                                <Input.Password disabled={true} placeholder='See "Change Password"'/>
-                                <Link href="/profile/change-password">
-                                    Change Password
-                                </Link>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Email"
-                                name="email"
-                                initialValue={email}
-                                rules={[
-                                    { required: true, message: 'Please input your email!' },
-                                    { validator: validateUgmEmail }
-                                ]}
-                            >
-                                <Input disabled={false}/>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="NIM"
-                                name="NIM"
-                                initialValue={NIM}
-                            >
-                                <Input disabled={false}/>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Github URL"
-                                name="githubURL"
-                                initialValue={github}
-                            >
-                                <Input disabled={false}/>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Bio"
-                                name="bio"
-                                initialValue={bio}
-                                rules={[
-                                    { max: 300, message: 'Bio must be less than 300 characters!' },
-                                ]}
-                            >
-                                <Input disabled={false}/>
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Verified"
-                                name="verified"
-                                initialValue={isVerified}
-                            >
-                                <Input disabled={true}/>
-                            </Form.Item>
-
-                            <Button
-                                type="primary"
-                                htmlType="submit"
-                                className="bg-green-2-500 rounded-full mb-0 font-bold"
-                                style={{ width: '100%', minHeight: '50px' }}
-                                onClick={updateProfile}
-                            >
-                                Save
-                            </Button>
-
-
-                            </ConfigProvider>
-                        </Form>
-                    )}
+        // Loading screen\
+        <FadeIn>
+            {loading ? (
+                <div className={`fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center bg-white visible`}>
+                    <LoadingScreen />
                 </div>
-            </div>
-        </div>
+            ) : (
+                <div className='flex flex-col font-sans bg-orange-100 text-neutral-1000 w-auto h-fill min-h-screen pt-24'>
+                        {/* Navbar */}
+                    <div className='flex flex-col h-fit bg-orange-100 text-neutral-1000'>
+                        <Head>
+                            <title>Profile</title>
+                            <meta name="description" content="Profile" />
+                            <link rel="icon" href="/favicon.ico" />
+                        </Head>
+                        <Navbar isAdmin={true}/>
+                    </div>
+
+                    <div className='flex justify-center align-middle items-center w-fill'>
+                        {/* Forms */}
+                        <div className="flex flex-col items-center justify-center w-5/6 h-fit min-h-full mb-10 py-7 bg-white rounded-2xl">
+                            <h2 className="text-4xl font-bold mb-5">Profile</h2>
+                                <Form
+                                    name="basic"
+                                    labelCol={{ span: 24 }}
+                                    wrapperCol={{ span: 24 }}
+                                    initialValues={{ remember: true }}
+                                    onValuesChange={(changedValues, allValues) => {
+                                        console.log(allValues);
+                                        setEmail(allValues.email);
+                                        setNIM(allValues.NIM);
+                                        setUsername(allValues.username);
+                                        setName(allValues.name);
+                                        setBio(allValues.bio);
+                                        setLocation(allValues.location);
+                                        setGithub(allValues.githubURL);
+                                    }}
+                                >
+                                    <ConfigProvider
+                                        theme={{
+                                            token: {
+                                                colorPrimary: '#48A516',
+                                            },
+                                        }}
+                                    >
+                                    {/* Avatar */}
+                                    <Form.Item
+                                        name="avatar"
+                                        initialValue={avatar}
+                                        className='flex justify-center'
+                                    >
+                                        {/* Rounded Image */}
+                                        <Image
+                                            src={avatar}
+                                            alt="Avatar"
+                                            width={200}
+                                            height={200}
+                                            className="rounded-full justify-center align-middle text-center flex items-center"
+                                        />
+                                        <style jsx global>{`
+                                            .ant-image-mask {
+                                                border-radius: 50%;
+
+                                            }
+                                        `}</style>
+                                    </Form.Item>
+                        
+                                    
+                                    {/* Create forms pre-filled with the data from the user */}
+                                    <Form.Item
+                                        label="ID"
+                                        name="id"
+                                        initialValue={id}
+                                    >
+                                        <Input disabled={true}/> 
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Username"
+                                        name="username"
+                                        initialValue={username}
+                                    >
+                                        <Input disabled={false}/>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Name"
+                                        name="name"
+                                        initialValue={name}
+                                    >
+                                        <Input disabled={false}/>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Password"
+                                        name="password"
+                                        initialValue=""
+                                    >
+                                        <Input.Password disabled={true} placeholder='See "Change Password"'/>
+                                        <Link href="/profile/change-password">
+                                            Change Password
+                                        </Link>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Email"
+                                        name="email"
+                                        initialValue={email}
+                                        rules={[
+                                            { required: true, message: 'Please input your email!' },
+                                            { validator: validateUgmEmail }
+                                        ]}
+                                    >
+                                        <Input disabled={false}/>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="NIM"
+                                        name="NIM"
+                                        initialValue={NIM}
+                                    >
+                                        <Input disabled={false}/>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Github URL"
+                                        name="githubURL"
+                                        initialValue={github}
+                                    >
+                                        <Input disabled={false}/>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Bio"
+                                        name="bio"
+                                        initialValue={bio}
+                                        rules={[
+                                            { max: 300, message: 'Bio must be less than 300 characters!' },
+                                        ]}
+                                    >
+                                        <Input disabled={false}/>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Verified"
+                                        name="verified"
+                                        initialValue={isVerified}
+                                    >
+                                        <Input disabled={true}/>
+                                    </Form.Item>
+
+                                    <Button
+                                        type="primary"
+                                        htmlType="submit"
+                                        className="bg-green-2-500 rounded-full mb-0 font-bold"
+                                        style={{ width: '100%', minHeight: '50px' }}
+                                        onClick={updateProfile}
+                                    >
+                                        Save
+                                    </Button>
+
+
+                                    </ConfigProvider>
+                                </Form>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </FadeIn>
     )
 }
 
